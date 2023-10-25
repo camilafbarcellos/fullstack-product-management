093e3e0a645d6e5e43977c0d35dd3f6a1f44dfce
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2023-10-25 00:28:06 -0300

    feaT: add menu component

---
+++ frontend/src/components/menu.tsx
@@ -0,0 +1,21 @@
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import ViewListRoundedIcon from '@mui/icons-material/ViewListRounded';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
+
+function Menu() {
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="a" sx={{ flexGrow: 1 }}>
+          Product Management
+        </Typography>
+        <Button startIcon={<ViewListRoundedIcon />} component={Link} to="/" color="inherit">List</Button>
+        <Button startIcon={<AddCircleRoundedIcon />} component={Link} to="/register" color="inherit">Register</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Menu;
