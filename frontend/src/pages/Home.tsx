50786e01557f5567fec5f6bf8291f48bbc43afbc
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2023-10-25 00:28:54 -0300

    feat: add home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Menu from '../components/menu';
+import ProductsTable from '../components/table';
+
+export default function Home() {
+
+    return (
+        <div>
+            <Menu />
+            <ProductsTable />
+        </div>
+    );
+}
