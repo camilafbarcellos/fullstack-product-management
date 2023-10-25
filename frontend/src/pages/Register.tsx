39641781e91a22cf3f74910eeb01c296efa2ce45
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2023-10-25 02:44:35 -0300

    feaT: add product register page

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from '../components/form';
+import Menu from '../components/menu';
+
+export default function Register() {
+
+  return (
+    <div style={{padding:"80px 10px 10px 10px"}}>
+      <Menu />
+      <RegisterForm />
+    </div>
+  );
+}
