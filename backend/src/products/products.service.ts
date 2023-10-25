092bf3775e5ad228dc9d02bc3c95dd78453a902c
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2023-10-25 16:22:10 -0300

    feat: add promo price calc at update

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -50,6 +50,30 @@ export class ProductsService {
             throw new NotFoundException('Product not found');
         }
 
+        // checks if the price changed without changing the promotional one
+        if (productDto.price !== product.price && productDto.promoPrice === product.promoPrice) {
+            // if so, automatically calculates the new promotional price
+            switch (product.category) {
+                case 'Smartphones':
+                    productDto.promoPrice = Number((productDto.price * 0.9745).toFixed(2)); // 2.55% discount
+                    break;
+                case 'Furniture':
+                    productDto.promoPrice = Number((productDto.price * 0.97).toFixed(2)); // 3% discount
+                    break;
+                case 'Electronics':
+                    productDto.promoPrice = Number((productDto.price * 0.957).toFixed(2)); // 4.3% discount
+                    break;
+                case 'Appliances':
+                    productDto.promoPrice = Number((productDto.price * 0.95).toFixed(2)); // 5% discount
+                    break;
+                case 'Refrigerators':
+                    productDto.promoPrice = Number((productDto.price * 0.925).toFixed(2)); // 7.5% discount
+                    break;
+                default:
+                    throw new Error('Invalid product category');
+            }
+        }
+
         Object.assign(product, productDto);
 
         return this.productsRepository.save(product);
