480ef6e3451716defda92e8b4101320df0ec624c
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2023-10-24 21:16:31 -0300

    feat: add category type and validator

---
+++ backend/src/util/CategoryValidator.ts
@@ -0,0 +1,5 @@
+import { ProductCategory } from "./ProductCategory";
+
+export function isValidCategory(category: string): boolean {
+  return Object.values(ProductCategory).includes(category as ProductCategory);
+}
