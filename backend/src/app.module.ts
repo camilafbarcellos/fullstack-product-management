37e9de80080274743865bf92702d1cd6a6cce510
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-08-08 21:14:20 -0300

    feat: add GraphQL module import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductsModule } from './products/products.module';
+import { ProductModule } from './products/product.module';
 import { Product } from './products/product.entity';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
       entities: [Product],
       synchronize: true,
     }),
-    ProductsModule,
+    ProductModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
