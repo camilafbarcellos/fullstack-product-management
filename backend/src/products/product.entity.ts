bcc8eb9a0fde44a453750c0fa61f61328852305b
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-08-08 21:17:11 -0300

    refactor: refactor product entity to GraphQL object

--- backend/src/products/product.entity.ts
+++ backend/src/products/product.entity.ts
@@ -1,27 +1,41 @@
+import { Field, ID, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { ProductCategory } from 'src/util/ProductCategory';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+registerEnumType(ProductCategory, {
+    name: 'ProductCategory',
+    description: 'The supported categories.',
+});
+
+@Entity() // TypeORM entity
+@ObjectType() // GraphQL object
 export class Product {
     @PrimaryGeneratedColumn()
+    @Field(() => ID)
     id: number;
 
     @Column()
+    @Field()
     name: string;
 
     @Column()
+    @Field()
     description: string;
 
     @Column()
+    @Field()
     color: string;
 
     // category controlled by enum
     @Column({ enum: ProductCategory })
-    category: string;
+    @Field(() => ProductCategory)
+    category: ProductCategory;
 
     @Column({ type: 'decimal', precision: 10, scale: 2 })
+    @Field()
     price: number;
 
     @Column({ type: 'decimal', precision: 10, scale: 2 })
+    @Field()
     promoPrice: number;
 }
